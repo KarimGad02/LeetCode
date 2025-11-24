3e28111e62cdcb30ff7d9db1bcac3f5c5a0c4a69
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-24 17:26:43 +0200

    Create 228-SummaryRanges

---
+++ 228-SummaryRanges/228-SummaryRanges.cpp
@@ -0,0 +1,33 @@
+// Last updated: 24/11/2025, 17:26:42
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums){
+        vector<string> result;
+        if (nums.empty()) return result;
+        if (nums.size() == 1){
+            result.push_back(to_string(nums[0]));
+            return result;
+        }
+        int left = 0;
+        int right = 1;
+        int control = 1;
+        while (right < nums.size()) {
+            if (nums[left] + control != nums[right]) {
+                if (control == 1)
+                    result.push_back(to_string(nums[left]));
+                else
+                    result.push_back(to_string(nums[left]) + "->" + to_string(nums[right - 1]));
+
+                left = right;
+                control = 0;
+            }
+            control++;
+            right++;
+        }
+        if (control == 1)
+            result.push_back(to_string(nums[left]));
+        else
+            result.push_back(to_string(nums[left]) + "->" + to_string(nums[right - 1]));
+        return result;
+    }
+};
