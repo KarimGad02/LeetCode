1624004370cfeb2e08e019cf815f5b36577fc3af
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-24 17:27:26 +0200

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.js
@@ -0,0 +1,20 @@
+// Last updated: 24/11/2025, 17:27:25
+var isValid = function (s) {
+    const closingBrackets = {
+        "(": ")",
+        "{": "}",
+        "[": "]",
+    };
+
+    const openingBrackets = [];
+
+    for (const bracket of s) {
+        if (bracket in closingBrackets) {
+            openingBrackets.push(bracket);
+        }
+        else if (closingBrackets[openingBrackets.pop()] !== bracket) {
+            return false;
+        }
+    }
+    return openingBrackets.length === 0;
+};
