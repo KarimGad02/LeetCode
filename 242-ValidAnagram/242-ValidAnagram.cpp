3bcea76081c67c5142c0b7db40b166df9476ddd1
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 17:18:26 +0200

    Delete 242-ValidAnagram directory

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++
@@ -1,9 +0,0 @@
-// Last updated: 28/11/2025, 13:48:02
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return (s == t);
-    }
-};
