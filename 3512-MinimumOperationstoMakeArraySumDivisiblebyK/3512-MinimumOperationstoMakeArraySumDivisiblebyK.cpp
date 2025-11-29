37ec7469cab731c69a7223b88f6fccf2e413ead4
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 16:15:55 +0200

    Create 3512-MinimumOperationstoMakeArraySumDivisiblebyK

---
+++ 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.cpp
@@ -0,0 +1,8 @@
+// Last updated: 29/11/2025, 16:15:56
+1class Solution {
+2public:
+3    int minOperations(vector<int>& nums, int k) {
+4        int sum = accumulate(nums.begin(), nums.end(), 0);
+5        return (sum % k);
+6    }
+7};
