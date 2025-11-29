ef5bfe562484161e0aed38339712a111328f4f42
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 16:26:47 +0200

    File updated at 29/11/2025, 16:26:48

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,17 +1,17 @@
-// Last updated: 24/11/2025, 17:27:29
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        outerloop:
-        for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i] + nums[j] == target && i != j){
-                    result[0]=i;
-                    result[1]=j;
-                    break outerloop;
-                }
-            }
-        }
-        return result;
-    }
-}
+// Last updated: 29/11/2025, 16:26:48
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int[] result = new int[2];
+4        outerloop:
+5        for(int i = 0; i < nums.length; i++){
+6            for(int j = 0; j < nums.length; j++){
+7                if(nums[i] + nums[j] == target && i != j){
+8                    result[0]=i;
+9                    result[1]=j;
+10                    break outerloop;
+11                }
+12            }
+13        }
+14        return result;
+15    }
+16}
