baca3517ef67b2385ccb545bccc19c9d315bbdfa
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 17:23:04 +0200

    Time: 135 ms (5.78%), Space: 48.3 MB (100%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let result = []
+    let L = 0
+    let R = nums.length-1
+
+    while(L<=R){
+        if (Math.abs(nums[L])>Math.abs(nums[R])){
+            result.unshift(Math.pow(nums[L], 2))
+            L+=1
+        } else{
+            result.unshift(Math.pow(nums[R], 2))
+            R-=1
+        }
+    }
+    return result
+};
