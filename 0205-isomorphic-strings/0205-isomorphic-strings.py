fa9e93bb384e7353a44f646baa9ea6d970b8deba
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 17:07:48 +0200

    Time: 39 ms (5.71%), Space: 16.6 MB (100%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        map = {}
+        memory = set()
+        for i in range(len(s)):
+            if s[i] in map:
+                if map[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in memory:
+                    return False
+                map[s[i]] = t[i]
+                memory.add(t[i])
+        return True
