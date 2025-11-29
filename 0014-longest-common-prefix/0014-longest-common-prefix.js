02985b8878feec32d13d8dce6c0bc10375dd2614
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 16:58:17 +0200

    Time: 48 ms (5.16%), Space: 42.1 MB (100%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let prefix = "";
+    for (let i = 0; i < strs[0].length; i++) {
+        if(strs.every(str => str[i] === strs[0][i])){ 
+            prefix += strs[0][i];
+        }else {
+            break;
+        }
+    }
+    return prefix;
+};
