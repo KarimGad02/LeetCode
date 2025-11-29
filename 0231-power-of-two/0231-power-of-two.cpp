2fd9942e7304ae880590c00cbcbea87b254f960f
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 17:09:16 +0200

    Time: 0 ms (100%), Space: 7.8 MB (82.22%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0)
+        if(n <= 0){
             return false;
-        return (n & (n-1)) == 0;
+            }
+        while(n % 2 == 0){
+            n = n/2;
+            }
+        return n==1;
     }
 };
