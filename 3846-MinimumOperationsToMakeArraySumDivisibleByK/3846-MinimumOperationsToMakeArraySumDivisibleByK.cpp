c558b8ff817a9ed94a1ddcd7062fd9f6b9e0b114
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 16:16:58 +0200

    Delete 3846-MinimumOperationsToMakeArraySumDivisibleByK directory

--- 3846-MinimumOperationsToMakeArraySumDivisibleByK/3846-MinimumOperationsToMakeArraySumDivisibleByK.cpp
+++
@@ -1,7 +0,0 @@
-// Last updated: 29/11/2025, 16:04:05
-class Solution {
-public:
-    int minOperations(vector<int>& nums, int k) {
-        return (accumulate(nums.begin(), nums.end(), 0) % k);
-    }
-};
