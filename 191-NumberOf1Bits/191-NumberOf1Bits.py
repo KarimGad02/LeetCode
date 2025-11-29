2126819f76fe12c36dc35fb4a5c5ad4f1f9d226e
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-11-29 17:16:52 +0200

    Delete 191-NumberOf1Bits directory

--- 191-NumberOf1Bits/191-NumberOf1Bits.py
+++
@@ -1,8 +0,0 @@
-# Last updated: 24/11/2025, 17:26:52
-class Solution:
-    def hammingWeight(self, n: int) -> int:
-        count = 0
-        while n > 0:
-            count += 1 if (n & 1) else 0
-            n >>= 1
-        return count
