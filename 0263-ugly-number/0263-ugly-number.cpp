edbe716c0faa2f292235eeb427ec9054e857b368
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2025-12-02 23:26:50 +0200

    Time: 0 ms (100%), Space: 7.7 MB (98.49%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -4,15 +4,13 @@ public:
         int factors[] = {2,3,5};
         int i = 0;
         if(n <= 0) return false;
-        if(n == 1) return true;
         while(i < 3){
             if(n % factors[i] == 0){
                 n /= factors[i];
                 i = -1;
             }
-            if(n == 1) return true;
             i++;
         }
-        return false;
+        return n==1;
     }
 };
